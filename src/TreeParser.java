4a20e6703ba6f6c049c855cd729e5c5572ac93cc
Theo Hornberger <theo.hornberger@telecomnancy.eu>
2023-12-09 15:07:20 +0100

    correctif DECL

--- src/TreeParser.java
+++ src/TreeParser.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -21,13 +20,15 @@ public class TreeParser {
 
     public void File() throws Exception {
         this.arbre = new Node("File");
-        Token current_token = tokens.peek();
+        // on recupere le premier token en le supprimant de la tete de file
+        Token current_token = tokens.poll();
+        // on recupere le tag du token
         Tag current_tag = current_token.getTag();
 
         //first token : with
         if(current_tag == Tag.WITH){
-            this.tokens.poll();
-            current_token = tokens.peek();
+            // le token est bon, on recupere le suivant en le supprimant de la tete de file
+            current_token = this.tokens.poll();
             current_tag = current_token.getTag();
         }else{
             throw new  SyntaxException(current_token.toString());
@@ -35,8 +36,7 @@ public class TreeParser {
 
         //second token : Ada
         if (current_tag == Tag.ID && ((Word) current_token).getValue()=="Ada"){
-            this.tokens.poll();
-            current_token = tokens.peek();
+            current_token = this.tokens.poll();
             current_tag = current_token.getTag();
         }else{
             throw new  SyntaxException(current_token.toString());
@@ -44,8 +44,7 @@ public class TreeParser {
 
         //third token : .
         if(current_tag==Tag.OP && ((Operator) current_token).getValue().equals("acs")){
-            this.tokens.poll();
-            current_token = tokens.peek();
+            current_token = this.tokens.poll();
             current_tag = current_token.getTag();
         }else{
             throw new SyntaxException(current_token.toString());
@@ -53,8 +52,7 @@ public class TreeParser {
 
         //fourth token : Text_IO
         if (current_tag == Tag.ID && ((Word) current_token).getValue()=="Text_IO"){
-            this.tokens.poll();
-            current_token = tokens.peek();
+            current_token = this.tokens.poll();
             current_tag = current_token.getTag();
         }else{
             throw new  SyntaxException(current_token.toString());
@@ -62,8 +60,7 @@ public class TreeParser {
 
         //fifth token : ;
         if(current_tag==Tag.SEPARATOR && ((Word) current_token).getValue().equals(";")){
-            this.tokens.poll();
-            current_token = tokens.peek();
+            current_token = this.tokens.poll();
             current_tag = current_token.getTag();
         }else{
             throw new  SyntaxException(current_token.toString());
@@ -71,15 +68,15 @@ public class TreeParser {
 
         //sixth token : use
          if(current_tag!=Tag.ID && ((Word) current_token).getValue().equals("use")){
-            this.tokens.poll();
-            current_token = tokens.peek();
+            current_token = this.tokens.poll();
             current_tag = current_token.getTag();
+        }else{
+            throw new  SyntaxException(current_token.toString());
         }
 
         //seventh token : Ada
         if (current_tag == Tag.ID && ((Word) current_token).getValue()=="Ada"){
-            this.tokens.poll();
-            current_token = tokens.peek();
+            current_token = this.tokens.poll();
             current_tag = current_token.getTag();
         }else{
             throw new  SyntaxException(current_token.toString());
@@ -87,8 +84,7 @@ public class TreeParser {
 
         //eighth token : .
         if(current_tag==Tag.OP && ((Operator) current_token).getValue().equals("acs")){
-            this.tokens.poll();
-            current_token = tokens.peek();
+            current_token = this.tokens.poll();
             current_tag = current_token.getTag();
         }else{
             throw new SyntaxException(current_token.toString());
@@ -96,8 +92,7 @@ public class TreeParser {
 
         //ninth token : Text_IO
         if (current_tag == Tag.ID && ((Word) current_token).getValue()=="Text_IO"){
-            this.tokens.poll();
-            current_token = tokens.peek();
+            current_token = this.tokens.poll();
             current_tag = current_token.getTag();
         }else{
             throw new  SyntaxException(current_token.toString());
@@ -105,66 +100,78 @@ public class TreeParser {
 
         //tenth token : ;
         if(current_tag==Tag.SEPARATOR && ((Word) current_token).getValue().equals(";")){
-            this.tokens.poll();
-            current_token = tokens.peek();
+            current_token = this.tokens.poll();
             current_tag = current_token.getTag();
         }else{
             throw new  SyntaxException(current_token.toString());
         }
 
         //eleventh token : procedure
-        if(current_tag==Tag.PROCEDURE){
-            this.tokens.poll();
-            current_token = tokens.peek();
-            current_tag = current_token.getTag();
-        }else{
-            throw new  SyntaxException(current_token.toString());
+        if(!(current_tag==Tag.PROCEDURE)){
+            //pas de poll car on veut garder le token pour le prochain appel de fonction
+             throw new  SyntaxException(current_token.toString());
         }
 
-        PROC();
-        
-        
+        Node proc = PROC();
+        // on ajoute le noeud de la procedure a l'arbre
+        this.arbre.addChild(proc);
         }
     
-    public void DECL() throws Exception {
-    
-        if(tokens.get(0).getTag()==Tag.TYPE){
-            Axiome.add(tokens.get(0)); 
-            tokens.remove(0);
-            IDENT(tokens);
-            DEF_IDENT(tokens);
+    public Node DECL() throws Exception {
+        Node decl = new Node("DECL");
+        // on recupere le premier token sans le supprimer car on veut le garder pour le prochain appel de fonction (ici IDENT_PLUS)
+        Token current_token = tokens.peek();
+        Tag current_tag = current_token.getTag();
+
+        // on regarde les premiers et les règles associées dans notre table puis on les applique
+
+        //first rule : type IDENT DEF_IDENT
+        if(current_tag == Tag.TYPE){
+            // on le supprime car on vient de l'utiliser
+            tokens.poll();
+            Node ident = IDENT(tokens);
+            Node def_ident = DEF_IDENT(tokens);
+            decl.addChild(ident);
+            decl.addChild(def_ident);
         }
-        if(tokens.get(0).getTag()==Tag.PROCEDURE){
-            Axiome.add(tokens.get(0)); 
-            tokens.remove(0);
-            PROC(tokens);
+        // second rule : procedure PROC
+        else if (current_tag == Tag.PROCEDURE){
+            tokens.poll();
+            Node proc = PROC(tokens);
+            decl.addChild(proc);
         }
-        if(tokens.get(0).getTag()==Tag.FUNCTION){
-            Axiome.add(tokens.get(0)); 
-            tokens.remove(0);
-            FUNC(tokens);
+        // third rule : function FUNC
+        else if (current_tag == Tag.FUNCTION){
+            tokens.poll();
+            Node func = FUNC(tokens);
+            decl.addChild(func);
+        }
+        // fourth rule : IDENT_PLUS : TYPE AFFECT_EXIST
+        else if (current_tag == Tag.ID){
+            Node ident_plus = IDENT_PLUS(tokens);
+            current_token = tokens.poll();
+            current_tag = current_token.getTag();
+            if(current_tag == Tag.OP && ((Operator) current_token).getValue().equals(":")){
+                Node type = TYPE(tokens);
+                Node affect_exist = AFFECT_EXIST(tokens);
+                decl.addChild(ident_plus);
+                decl.addChild(type);
+                decl.addChild(affect_exist);
+            }else{
+                throw new SyntaxException(current_token.toString());
+            }
         }
         else{
-            
-            IDENT_PLUS(tokens);
-            if(tokens.get(0) instanceof Word || ((Word) Axiome.get(0)).getValue()!=":"){
-                Axiome.add(tokens.get(0)); 
-                tokens.remove(0);
-                if(tokens.get(0).getTag()==Tag.TYPE){
-                    Axiome.add(tokens.get(0)); 
-                    tokens.remove(0);
-                    AFFECT_EXIST(tokens);
-                    if(tokens.get(0).getTag()==Tag.SEPARATOR){
-                        Axiome.add(tokens.get(0)); 
-                        tokens.remove(0);}
-                }
-            }
-            
-
+            throw new SyntaxException(current_token.toString());
         }
+        return decl;
     }
 
-    public void DEF_IDENT() throws Exception{
+    private Node IDENT(ArrayDeque<Token> tokens2) {
+        return null;
+    }
+
+    public Node DEF_IDENT() throws Exception{
         if(tokens.get(0).getTag()==Tag.SEPARATOR){
             Axiome.add(tokens.get(0)); 
             tokens.remove(0);}
@@ -175,7 +182,7 @@ public class TreeParser {
         else{throw new Exception("Syntax Error");}
     }
 
-    public void DEF_IDENT_FIN() throws Exception {
+    public Node DEF_IDENT_FIN() throws Exception {
         if(tokens.get(0).getTag()==Tag.ACCESS){
             Axiome.add(tokens.get(0)); 
             tokens.remove(0);
@@ -204,7 +211,7 @@ public class TreeParser {
 
     }
 
-    public void PROC() throws Exception {
+    public Node PROC() throws Exception {
     
     }
     }
