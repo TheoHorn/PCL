cd8e6694755cbf61b9d707bbb000773dda743809
FONDRAT Julie <julie.fondrat@telecomnancy.eu>
2024-01-18 15:07:48 +0000

    Update DeclTest.java

--- test/java/SyntaxerTest/DeclTest.java
+++ test/java/SyntaxerTest/DeclTest.java
@@ -34,7 +34,7 @@ public class DeclTest {
         syntaxer.addToken(new Word(Tag.SEPARATOR,";"));
         syntaxer.addToken(new Word(Tag.END, "end"));
         //IDENT_EXIST
-        syntaxer.addToken(new Word(Tag.ID, "test"));  // Example for IDENT_EXISTE, adjust based on your grammar
+        syntaxer.addToken(new Word(Tag.ID, "test")); 
         syntaxer.addToken(new Word(Tag.SEPARATOR, ";"));
         Node node = syntaxer.DECL();
         Node node2 = new Node("test");
@@ -123,81 +123,6 @@ public class DeclTest {
         syntaxer.addToken(new Word(Tag.ID, "test"));
     }
 
-
-//PROC()
-    @Test
-    public void procedure() throws Exception {
-        syntaxer.addToken(new Word(Tag.ID, "test"));
-        syntaxer.addToken(new Word(Tag.IS, "is"));
-        //DECL_MULT
-        syntaxer.addToken(new Word(Tag.BEGIN, "begin"));
-        //INSTR_PLUS
-        syntaxer.addToken(new Word(Tag.ID,"ident"));
-        syntaxer.addToken(new Word(Tag.SEPARATOR,";"));
-        syntaxer.addToken(new Word(Tag.END, "end"));
-        //IDENT_EXIST
-        syntaxer.addToken(new Word(Tag.ID, "test"));  // Example for IDENT_EXISTE, adjust based on your grammar
-        syntaxer.addToken(new Word(Tag.SEPARATOR, ";"));
-        Node node = syntaxer.PROC();
-        Node node2 = new Node("test");
-        
-        assertEquals(node2, node.getChildren().get(0));
-    }
-
-    @Test
-    public void test_procedure_error_begin() throws Exception {
-        syntaxer.addToken(new Word(Tag.ID, "test"));
-        syntaxer.addToken(new Word(Tag.IS, "is"));
-        //DECL_MULT
-        //INSTR_PLUS
-        syntaxer.addToken(new Word(Tag.END, "end"));
-        //IDENT_EXIST
-        syntaxer.addToken(new Word(Tag.ID, "test"));  // Example for IDENT_EXISTE, adjust based on your grammar
-        syntaxer.addToken(new Word(Tag.SEPARATOR, ";"));
-        assertThrows(SyntaxException.class, () -> syntaxer.PROC());
-    }
-
-    @Test
-    public void test_procedure_error_end() throws SyntaxException {
-        syntaxer.addToken(new Word(Tag.ID, "test"));
-        syntaxer.addToken(new Word(Tag.IS, "is"));
-        //DECL_MULT
-        syntaxer.addToken(new Word(Tag.BEGIN, "begin"));
-        //INSTR_PLUS
-        //IDENT_EXIST
-        syntaxer.addToken(new Word(Tag.ID, "test"));  // Example for IDENT_EXISTE, adjust based on your grammar
-        syntaxer.addToken(new Word(Tag.SEPARATOR, ";"));
-        assertThrows(NullPointerException.class, () -> syntaxer.PROC());
-    }
-
-    @Test
-    public void test_procedure_error_separator() throws Exception {
-        syntaxer.addToken(new Word(Tag.ID, "test"));
-        syntaxer.addToken(new Word(Tag.IS, "is"));
-        //DECL_MULT
-        syntaxer.addToken(new Word(Tag.BEGIN, "begin"));
-        //INSTR_PLUS
-
-        syntaxer.addToken(new Word(Tag.END, "end"));
-        //IDENT_EXIST
-        syntaxer.addToken(new Word(Tag.ID, "test"));  // Example for IDENT_EXISTE, adjust based on your grammar
-
-        assertThrows(SyntaxException.class, () -> syntaxer.PROC());
-    }
-
-
-    @Test
-    public void test_procedure_error() throws Exception {
-        //mettre un autre Tag que celui attendu pour vérifier la détection d'erreur
-        syntaxer.addToken(new Word(Tag.ID,"test"));
-        syntaxer.addToken(new Word(Tag.OR,"or"));
-        //assertThrows(SyntaxException.class, () -> syntaxer.PROC());
-    }
-
-     @Test
-     public void test_ajout_instruction(){
-
-     }
 
     @Test
     public void test_identificateur_exist_error() throws Exception {
